c5ac3d81c706e99f4b0e8103516808c36f7ce970
shubham dave <daveshubham611@gmail.com>
2020-11-20 11:51:05 +0530

    files ended

---
+++ common/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	 <meta charset="utf-8">
+  	 <meta name="viewport" content="width=device-width, initial-scale=1">
+  	 <title></title>
+  	   <meta content="" name="description">
+  <meta content="" name="keywords">
+
+	<!--  <link href="assets/vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+	<!--  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	 <link rel="stylesheet" type="text/css" href="assets/style.css">
+
+	  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+	 <link rel="stylesheet" href="assets/vendors/bootstrap/dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="assets/vendors/PACE/themes/blue/pace-theme-minimal.css" />
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/css/perfect-scrollbar.min.css" />
+
+    <!-- vendor css -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+</head>
+<body>
+	<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="">Arsha</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="#">Product</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <a href="#about" class="get-started-btn scrollto">Get Started</a>
+
+    </div>
+  </header>
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <script src="assets/js/main.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,312 @@
+<?php include 'common\header.php'?>
+<section id="hero" class="d-flex align-items-center">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+				<h1>Better Solutions For Your Business</h1>
+				<h2>We are team of talanted designers making websites with Bootstrap</h2>
+				<div class="d-lg-flex">
+					<a href="#about" class="btn-get-started scrollto">Get Started</a>
+					<a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
+				</div>
+			</div>
+			<div class="col-lg-6  order-1 order-lg-2">
+				<div id="demo" class="carousel slide" data-ride="carousel">
+
+					<!-- Indicators -->
+					<ul class="carousel-indicators">
+						<li data-target="#demo" data-slide-to="0" class="active"></li>
+						<li data-target="#demo" data-slide-to="1"></li>
+						<li data-target="#demo" data-slide-to="2"></li>
+					</ul>
+
+					<!-- The slideshow -->
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="assets/img/liquied.jpg" alt="sodium silicate liquid" width="800" height="300">
+							<div class="carousel-caption">
+								<h3 class="text-dark">liquid</h3>
+								<p class="text-dark">sodium silicate liquid</p>
+							</div> 
+						</div>
+						<div class="carousel-item">
+							<img src="assets/img/solid.jpg" alt="sodium silicate solid" width="800" height="300">
+							<div class="carousel-caption">
+								<h3 class="text-dark">liquid</h3>
+								<p class="text-dark">sodium silicate solid</p>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img src="assets/img/liquied.jpg" alt="sodium meta silicate" width="800" height="300">
+							<div class="carousel-caption">
+								<h3 class="text-dark">liquid</h3>
+								<p class="text-dark">sodium meta silicate</p>
+							</div>
+						</div>
+					</div>
+
+					<!-- Left and right controls -->
+					<a class="carousel-control-prev" href="#demo" data-slide="prev">
+						<span class="carousel-control-prev-icon"></span>
+					</a>
+					<a class="carousel-control-next" href="#demo" data-slide="next">
+						<span class="carousel-control-next-icon"></span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--about-->
+<section id="about" class="about">
+	<div class="container" data-aos="fade-up">
+
+		<div class="section-title">
+			<h2>About Us</h2>
+		</div>
+
+		<div class="row content">
+			<div class="col-lg-6">
+				<p style="font-size: 20px">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+				magna aliqua.</p>
+				<ul>
+					<li style="font-size: 15px"><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+					<li style="font-size: 15px"><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
+					<li style="font-size: 15px"><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+				</ul>
+			</div>
+			<div class="col-lg-6 pt-4 pt-lg-0">
+				<p style="font-size: 18px">
+					Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+					velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="#" class="btn-learn-more">Learn More</a>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+<!-- -->
+<section id="services" class="services section-bg">
+	<div class="container" data-aos="fade-up">
+
+		<div class="section-title">
+			<h2>Our Product</h2>
+		</div>
+
+		<div class="row content">
+			<div class="col-sm-3 col-md-3 feature_teaser" data-aos="zoom-in" data-aos-delay="100">
+				<img alt="responsive" src="assets/img/liquied.jpg">
+				<h3>SODIUM  SILICATE SOLID</h3>
+				<p style="font-size: 15px">Sodium Silicate is one of most branched out and specialized product with its applications ranging to suit different products.<br><br><br></p>
+			</div>
+			<div class="col-sm-3 col-md-3 feature_teaser" data-aos="zoom-in" data-aos-delay="100">
+				<img alt="responsive" src="assets/img/solid.jpg">
+				<h3>SODIUM  SILICATE SOLID</h3>
+				<p style="font-size: 15px">Sodium Silicate is one of most branched out and specialized product with its applications ranging to suit different products.<br><br><br></p>
+			</div>
+			<div class="col-sm-3 col-md-3 feature_teaser" data-aos="zoom-in" data-aos-delay="100">
+				<img alt="responsive" src="assets/img/liquied.jpg">
+				<h3>SODIUM  SILICATE SOLID</h3>
+				<p style="font-size: 15px">Sodium Silicate is one of most branched out and specialized product with its applications ranging to suit different products.<br><br><br></p>
+			</div>
+			<div class="col-sm-3 col-md-3 feature_teaser" data-aos="zoom-in" data-aos-delay="100">
+				<img alt="responsive" src="assets/img/liquied.jpg">
+				<h3>SODIUM  SILICATE SOLID</h3>
+				<p style="font-size: 15px">Sodium Silicate is one of most branched out and specialized product with its applications ranging to suit different products.<br><br><br></p>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+<!--Team Section-->
+<section id="team" class="team section-bg">
+	<div class="container" data-aos="fade-up">
+
+		<div class="section-title">
+			<h2>Team</h2>
+			<p style="font-size: 15px">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+		</div>
+
+		<div class="row">
+
+			<div class="col-lg-6">
+				<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+					<div class="pic"><img src="assets/img/team-1.jpg" class="img-fluid" alt=""></div>
+					<div class="member-info">
+						<h4>Nirav Patel</h4>
+						<span>Chief Executive Officer</span>
+						<p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+						<div class="social">
+							<a href=""><i class="ri-twitter-fill"></i></a>
+							<a href=""><i class="ri-facebook-fill"></i></a>
+							<a href=""><i class="ri-instagram-fill"></i></a>
+							<a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-6 mt-4 mt-lg-0">
+				<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
+					<div class="pic"><img src="assets/img/team-3.jpg" class="img-fluid" alt=""></div>
+					<div class="member-info">
+						<h4>Mitul Patel</h4>
+						<span>Product Manager</span>
+						<p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+						<div class="social">
+							<a href=""><i class="ri-twitter-fill"></i></a>
+							<a href=""><i class="ri-facebook-fill"></i></a>
+							<a href=""><i class="ri-instagram-fill"></i></a>
+							<a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-6 mt-4">
+				<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="300">
+					<div class="pic"><img src="assets/img/team-1.jpg" class="img-fluid" alt=""></div>
+					<div class="member-info">
+						<h4>Shubham Dave</h4>
+						<span>CTO</span>
+						<p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+						<div class="social">
+							<a href=""><i class="ri-twitter-fill"></i></a>
+							<a href=""><i class="ri-facebook-fill"></i></a>
+							<a href=""><i class="ri-instagram-fill"></i></a>
+							<a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-6 mt-4">
+				<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
+					<div class="pic"><img src="assets/img/team-3.jpg" class="img-fluid" alt=""></div>
+					<div class="member-info">
+						<h4>Jay Patel</h4>
+						<span>Accountant</span>
+						<p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+						<div class="social">
+							<a href=""><i class="ri-twitter-fill"></i></a>
+							<a href=""><i class="ri-facebook-fill"></i></a>
+							<a href=""><i class="ri-instagram-fill"></i></a>
+							<a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</section>
+
+<!-- -->
+<section id="pricing" class="pricing">
+	<div class="container" data-aos="fade-up">
+
+		<div class="section-title">
+			<h2>Company Profile</h2>
+			<p style="font-size: 20px">Advatech is a venture, conceptualized and promoted by Nandolia family,
+			which was establised in 1998 and became one of the leading manufacturer far sodium silicate located at state Gujarat of india.</p>
+
+			<p style="font-size: 20px">With annual capacity of 60000 MT. of silicate glass , 50000 MT. of silicate liquid and 5000 MT of Sodium Meta SIlicate.
+				Advatech is a ISO 9001 Registered India based private limited company, having adopted a set of values and
+			principles on which its groups reputation and respectability have been built over the past years.</p>
+
+			<p style="font-size: 20px">Advatech Industries Pvt Ltd. currently is meeting its requirement of energy from ONGC with daily allotment
+				of 25000 to 35000 SCMD natural Gas making it one of the second largest gas consumer of ONGC LTD.
+			in Mehsana region of North Gujarat.</p>
+
+			<p style="font-size: 20px">We use state-of art machinery & equipments in the manufacturing process with a firm commitment and focus on to always produce
+			Best Quality Products to enhance Customer Satisfaction.</p>
+
+			<p style="font-size: 20px">At present, we serve the Adhesives, Building Materials & Construction, Soaps & Detergent, Foundry & Foundry Chemicals, Mineral Processing,
+			Pulp & Paper, Paints, Refractories, Textile Processing, Titanium Dioxides, Silica & Allied Chemical Industries.</p>
+		</div>
+	</div>
+</section>
+
+
+<section id="contact" class="contact">
+	<div class="container" data-aos="fade-up">
+
+		<div class="section-title">
+			<h2>Contact</h2>
+			<p style="font-size: 15px">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+		</div>
+
+		<div class="row">
+
+			<div class="col-lg-5 d-flex align-items-stretch">
+				<div class="info">
+					<div class="address">
+						<i class="icofont-google-map"></i>
+						<h4>Location:</h4>
+						<p>A108 Adam Street, New York, NY 535022</p>
+					</div>
+
+					<div class="email">
+						<i class="icofont-envelope"></i>
+						<h4>Email:</h4>
+						<p>info@example.com</p>
+					</div>
+
+					<div class="phone">
+						<i class="icofont-phone"></i>
+						<h4>Call:</h4>
+						<p>+1 5589 55488 55s</p>
+					</div>
+
+					<iframe width="400" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=mehsana&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+				</div>
+
+			</div>
+
+			<div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+				<form action="forms/contact.php" method="post" role="form" class="php-email-form">
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="name">Your Name</label>
+							<input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+							<div class="validate"></div>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="name">Your Email</label>
+							<input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+							<div class="validate"></div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="name">Subject</label>
+						<input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+						<div class="validate"></div>
+					</div>
+					<div class="form-group">
+						<label for="name">Message</label>
+						<textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+						<div class="validate"></div>
+					</div>
+					<div class="mb-3">
+						<div class="loading">Loading</div>
+						<div class="error-message"></div>
+						<div class="sent-message">Your message has been sent. Thank you!</div>
+					</div>
+					<div class="text-center"><button type="submit">Send Message</button></div>
+				</form>
+			</div>
+
+		</div>
+
+	</div>
+</section>
+
+<a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+<div id="preloader"></div>
+<?php include 'common\footer.php'?>
